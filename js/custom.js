7408f89e7a4a1feba3c806c433e2972c16814a27
Heet <64312275+Heet09@users.noreply.github.com>
2024-07-01 17:33:36 -0400

    Delete js directory

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-function toggleDescription() {
-    var description = document.getElementById('description');
-    if (description.classList.contains('hidden')) {
-        description.classList.remove('hidden');
-    } else {
-        description.classList.add('hidden');
-    }
-}
